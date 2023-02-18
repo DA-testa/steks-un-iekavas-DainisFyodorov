4ba29c296ad195f9479b002ae1914d23356ad508
DainisFyodorov <fedorovdainis@gmail.com>
2023-02-18 23:11:03 +0200

    upd

--- main.py
+++ main.py
@@ -14,14 +14,10 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next,i))
-            
-
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
-            
-            
     if opening_brackets_stack:
         return opening_brackets_stack[-1].position + 1 
     return "Success"
